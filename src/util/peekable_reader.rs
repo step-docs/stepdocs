9f3d14ca5e31b756e5d2dedba86199941dcf0df2
wireless4024 <wireless4024@proton.me>
2022-10-16 23:59:17 +0700

    peekable async buf reader

--- src/util/peekable_reader.rs
+++ src/util/peekable_reader.rs
@@ -38,11 +38,26 @@ impl<T: AsyncRead + Unpin> PeekableLine<T> {
 		})
 	}
 
+	pub fn consume_peek(&mut self) {
+		self.next = None;
+	}
+
+	pub async fn next_line(&mut self, capacity: usize) -> io::Result<String> {
+		let mut buf = String::with_capacity(capacity);
+		self.read_line(&mut buf).await?;
+		Ok(buf)
+	}
+
 	pub async fn peek_line(&mut self) -> io::Result<&str> {
 		if self.next.is_some() {
 			Ok(self.next.as_ref().unwrap())
 		} else {
-			self.next = Some(String::new());
+			let mut buf = String::new();
+			let len = self.inner.read_line(&mut buf).await?;
+			if len == 0 {
+				return Ok("");
+			}
+			self.next = Some(buf);
 			Ok(self.next.as_ref().unwrap())
 		}
 	}
